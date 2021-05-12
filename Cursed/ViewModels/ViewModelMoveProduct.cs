23cf39f2d54868ac7bfd2e72f1346ffa7332bff2
Андрей Когут <joker320kingkobra@mail.ru>
2021-05-12 21:55:52 +1000

    надо доработать поиск в перемещении продукта

--- Cursed/ViewModels/ViewModelMoveProduct.cs
+++ Cursed/ViewModels/ViewModelMoveProduct.cs
@@ -6,10 +6,11 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Windows.Data;
 
 namespace Cursed.ViewModels
 {
-    public class ViewModelMoveProduct : INotifyPropertyChanged
+    public class ViewModelMoveProduct : ViewModelBase, INotifyPropertyChanged
     {
         DB DB;
 
@@ -20,27 +21,37 @@ namespace Cursed.ViewModels
         void SignalChanged([CallerMemberName] string prop = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
         public MiniCommand MoveProduct { get; set; }
 
-        public ObservableCollection<Product> Numbers { get; set; }//new
         public ViewModelMoveProduct()
         {
             DB = DB.GetDb();
             PartOfWarehouses = new ObservableCollection<PartOfWarehouse>(DB.PartOfWarehouses);
+
+            #region Фильтр, поиск
+            Products = new ObservableCollection<Product>(DB.Products);//
+            EmployeesCollectionView = CollectionViewSource.GetDefaultView(Products);
+            EmployeesCollectionView.Filter = FilterEmployees;
+            //EmployeesCollectionView.GroupDescriptions.Add(new PropertyGroupDescription(nameof(Product.Unit))); // эта сортировка группирует по единице товара
+            EmployeesCollectionView.SortDescriptions.Add(new SortDescription(nameof(Product.Code), ListSortDirection.Ascending)); // это сортировка по коду товара
+
+            #endregion
+
             MoveProduct = new MiniCommand(() =>
             {
-                DB.PartOfWarehouses.Add(selectedProduct.PartOfWarehouse); // проверить все binding в front'e
-                //DB.
-                //if(selectedPartOfWarehouse2 != null && selectedPartOfWarehouse != null)
-                // {
-               // selectedPartOfWarehouse.Add(Numbers);  КАК ЗАСТАВИТЬ ЭТУ КНОПКУ РАБОТАТЬ
-                 //   DB.PartOfWarehouses.Add(selectedPartOfWarehouse2);
-                 //   DB.SaveChanges();
-                //}
-               // else
-               // {
-               //     System.Windows.MessageBox.Show("Заполните все поля");
-               // }
+                if (SelectedPartOfWarehouse != null && SelectedPartOfWarehouse2 != null && SelectedPartOfWarehouse2 != SelectedPartOfWarehouse && SelectedProduct != null)
+                {
+                    SelectedProduct.PartOfWarehouse = SelectedPartOfWarehouse2;
+
+                    DB.SaveChanges();
+                }
+                else
+                {
+                    System.Windows.MessageBox.Show("Заполните все поля");
+                }
             });
         }
+
+        #region selected properties
+
         private Product selectedProduct;
         public Product SelectedProduct
         {
@@ -61,7 +72,7 @@ namespace Cursed.ViewModels
                 selectedPartOfWarehouse = value;
                 try
                 {
-                    if (selectedPartOfWarehouse.Products == null)
+                    if (selectedPartOfWarehouse.Products == null || selectedPartOfWarehouse.Products.Count == 0)
                     {
                         System.Windows.MessageBox.Show("Вы выбрали пустой отсек");
                     }
@@ -82,9 +93,48 @@ namespace Cursed.ViewModels
             set
             {
                 selectedPartOfWarehouse2 = value;
-                //SignalChanged("Products"); нам не надо
-                System.Windows.MessageBox.Show("Вы выбрали");
             }
         }
+        #endregion
+
+        #region поляна для поиска
+
+        public ICollectionView EmployeesCollectionView { get; set; }
+
+        private string _employeesFilter = string.Empty;
+        public string EmployeesFilter
+        {
+            get
+            {
+                return _employeesFilter;
+            }
+            set
+            {
+                _employeesFilter = value;
+                OnPropertyChanged(nameof(EmployeesFilter));
+                EmployeesCollectionView.Refresh();
+            }
+        }
+        private bool FilterEmployees(object obj)
+        {
+            if (obj is Product product)
+            {
+                return product.Name.Contains(EmployeesFilter, StringComparison.InvariantCultureIgnoreCase) || product.Unit.Contains(EmployeesFilter, StringComparison.InvariantCultureIgnoreCase);
+            }
+            return false;
+        }
+
+        // Метод ниже обновляет листвью, чтобы работал поиск после редактирования листвью. (перезаписывает в icollection уже отредаченный список и поиск снова работает)
+        private void RefreshListView()
+        {
+            EmployeesCollectionView = CollectionViewSource.GetDefaultView(Products);
+            EmployeesCollectionView.Filter = FilterEmployees;
+            //EmployeesCollectionView.GroupDescriptions.Add(new PropertyGroupDescription(nameof(Product.Unit)));
+            EmployeesCollectionView.SortDescriptions.Add(new SortDescription(nameof(Product.Code), ListSortDirection.Ascending));
+        }
+
+        #endregion
+
     }
+
 }
